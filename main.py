c4171337af08c1aca4d08c8a85a32246ea6e2f1c
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-06-25 11:36:34 +0700

    Add back

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ from game.side_selector import SideSelectionScreen
 WIDTH, HEIGHT = 640, 640
 
 def main():
+    
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH + 300, HEIGHT))
+    screen = pygame.display.set_mode((WIDTH + 300, HEIGHT + 20))
     pygame.display.set_caption("Chess with Prolog")
     font = pygame.font.SysFont("arial", 20)
 
-    # Show side selection
-    selector = SideSelectionScreen(screen, font)
-    player_color = selector.get_selection()
+    while True:
+        selector = SideSelectionScreen(screen, font)
+        player_color = selector.get_selection()
 
-    # Start game with selected side
-    game = Game(screen=screen, font=font, player_color=player_color)
-    game.run()
+        if player_color == "exit":
+            break
+
+        game = Game(screen=screen, font=font, player_color=player_color)
+        game.run()
+    
 
 if __name__ == "__main__":
     main()
